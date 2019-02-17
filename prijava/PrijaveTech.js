dd917d55a80203d3b95b29a41e12521a6dc6cd5e
petkostefan <petkostefan@gmail.com>
2019-02-17 13:40:31 +0100

    napravljena prijava

---
+++ prijava/PrijaveTech.js
@@ -0,0 +1,37 @@
+$(function () {	
+
+	//COLLAPSIBLE
+
+	var coll = document.getElementsByClassName("collapsible");
+	var i;
+	var l;
+
+	for (i = 0; i < coll.length; i++) {
+		coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			for (l =0 ; l < coll.length; l++){
+				var cont = coll[l].nextElementSibling;
+				cont.style.display = 'none'
+			}
+			content.style.display = "block";
+		}
+		});
+	}
+
+	//GOOGLE SHEET
+
+	const scriptURL = 'https://script.google.com/macros/s/AKfycbw9f30MBIAtp9BzqTv-vCNIOO4TJOVuzWs5hElbtHPQIMpmCcJo/exec'
+	const form = document.forms['submit-to-google-sheet']
+
+	form.addEventListener("submit", e => {
+		e.preventDefault()
+		fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+			.then(response => console.log('Success!', response))
+			.catch(error => console.error('Error!', error.message))
+	})
+
+});
