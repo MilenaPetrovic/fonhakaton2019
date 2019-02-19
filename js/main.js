b5c2ebdb4730d7545e5ca796f5f57190db3e2065
nikolasimeunovic <sime97kv@gmail.com>
2019-02-19 01:00:22 +0100

    dodati tekstovi za timeline

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var glide = new Glide('.glide', {
     startAt: 5,
     perView: 1,
     focusAt: 'center',
-    dragThreshold: 30,
+    dragThreshold: 60,
     animationDuration: 500,
     // peek: -48,
     gap: 0,
